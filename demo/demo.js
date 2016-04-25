3759a67f5390752981231778101150735ac01805
Ned Letcher <nletcher@gmail.com>
2016-04-25 19:51:39 +1000

    added image downloading support

--- demo/demo.js
+++ demo/demo.js
@@ -7,7 +7,7 @@
 var ERG_URL = 'http://erg.delph-in.net/rest/0.9/parse';
 
 // Makes a few things easier for development
-var DEV_MODE = false;
+var DEV_MODE = true;
 
 
 // Using underscore.js/lodash.js  Templates
@@ -17,8 +17,8 @@ Templates.result = [
     '<div class="result">',
         '<div class="result-inner">',
             '<div class="num"><%= resultNum %></div>',
-            '<div class="tree"></div>',
-            '<div class="mrs"></div>',
+            '<div class="tree viz"><div class="tools hidden"><div class="save"></div></div></div>',
+            '<div class="mrs viz"><div class="tools hidden"><div class="save"></div></div></div>',
         '</div>',
     '</div>'].join("\n");
 
@@ -34,7 +34,88 @@ for (var template in Templates) {
         Templates[template] = _.template(Templates[template]);
     }
 }
-   
+
+
+function Result(result, parent) {
+    // Create this object
+    var self = {
+        data : result,
+        num : result['result-id'] + 1,
+        drawTree : drawTree,
+        drawMrs : drawMrs
+    };
+
+    // Create and attach the DOM element that will contain the Result
+    var $result = $(Templates.result({'resultNum': self.num})).appendTo(parent);
+
+    // Add data structures as per the available data
+    if (self.data.derivation)
+        var treeSvg = self.drawTree(self.data.derivation, $result.find('.tree')[0]);
+    if (self.data.mrs)
+        var mrsSvg = self.drawMrs(self.data.mrs, $result.find('.mrs')[0]);
+
+    // Add hover listeners for trees and MRS
+    $result.find('.tree, .mrs').hover(
+        function(event) {
+            $(this).find('.tools').removeClass('hidden');
+        },
+        function(event) {
+            $(this).find('.tools').addClass('hidden');
+        }
+    );
+
+    // TODO: move the callback to named function
+    $result.find('.save').click(function (event){
+        // Save SVG to a canvas
+        var $canvas = $('<canvas>');
+        var canvas = $canvas[0];
+        var ctx = canvas.getContext('2d');
+        var $svg = $(this).closest('.viz').find('svg');
+        ctx.canvas.height = $svg.height();
+        ctx.canvas.width = $svg.width();
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0 ,0, ctx.canvas.width, ctx.canvas.height);
+        
+        // convert SVG to dataUrl
+        var data = (new XMLSerializer()).serializeToString($svg[0]);
+        var DOMURL = window.URL || window.webkitURL || window;
+
+        var img = new Image();
+        var svgBlob = new Blob([data], {type: 'image/svg+xml;charset=utf-8'});
+        var url = DOMURL.createObjectURL(svgBlob);
+
+        img.onload = function () {
+            ctx.drawImage(img, 0, 0);
+            DOMURL.revokeObjectURL(url);
+
+            var imgURI = canvas
+                    .toDataURL('image/png')
+                    .replace('image/png', 'image/octet-stream');
+
+            // on the image load, actually download it
+            triggerDownload(imgURI);
+        };
+        img.src = url;
+    });
+
+    return self;
+}
+
+
+function triggerDownload (imgURI) {
+    var evt = new MouseEvent('click', {
+        view: window,
+        bubbles: false,
+        cancelable: true
+    });
+
+    var a = document.createElement('a');
+    a.setAttribute('download', 'delphin.png');
+    a.setAttribute('href', imgURI);
+    a.setAttribute('target', '_blank');
+    a.dispatchEvent(evt);
+}
+
 
 function doResults(data){
     var results = data.results;
@@ -47,19 +128,10 @@ function doResults(data){
         .appendTo($('#results-info').empty());
     
     //Create and add the results
-    var $parent = $('#results-container').empty();
+    var parent = $('#results-container').empty()[0];
     
     for (var i=0; i < results.length; i++) {
-        var result = results[i];
-        var $result = $(Templates.result({'resultNum': i+1})).appendTo($parent);
-
-        if (result.derivation) {
-            var svg = drawTree(result.derivation, $result.find('.tree')[0]);
-        }
-        
-        if (result.mrs) {
-            var svg = drawMrs(result.mrs, $result.find('.mrs')[0]);
-        }   
+        var result = Result(results[i], parent);
     }
 }
 
